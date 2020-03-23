69d20ceb4b6ee0bfef121b4bacc00e83d874b757
jchung201 <jchung201@gmail.com>
2020-03-23 13:07:31 -0400

    (User) Duplicate error handling.

--- src/auth/models/user.repository.ts
+++ src/auth/models/user.repository.ts
@@ -1,6 +1,10 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDTO } from '../dtos/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,6 +13,12 @@ export class UserRepository extends Repository<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505')
+        throw new ConflictException('Username already exists!');
+      else throw new InternalServerErrorException();
+    }
   }
 }
