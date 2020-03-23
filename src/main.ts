dc579f70b61455114d398ef6abb2b2e5b19cb320
jchung201 <jchung201@gmail.com>
2020-03-23 19:57:34 -0400

    (CORS) Activated if on development mode.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') app.enableCors();
+
+  const serverConfig = config.get('server');
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
