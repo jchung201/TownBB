65ccd030c3329099040751b77ca87a484e2fd06f
jchung201 <jchung201@gmail.com>
2020-03-23 13:00:53 -0400

    (Auth) Basic validators on username/password.:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -7,7 +7,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body() authCredentialsDTO: AuthCredentialsDTO) {
+  signUp(@Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO) {
     return this.authService.signUp(authCredentialsDTO);
   }
 }
