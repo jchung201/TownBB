fb6fd013b2a1c891bb59bd70c46d872d53b902d9
jchung201 <jchung201@gmail.com>
2020-03-26 00:55:18 -0400

    (ENV) Typeorm setup done.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,24 +1,42 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as config from 'config';
 
-const dbConfig = config.get('db');
-const {
-  type,
-  host,
-  port,
-  username,
-  password,
-  database,
-  synchronize,
-} = dbConfig;
+// DB
+let RDS_TYPE;
+let RDS_HOSTNAME;
+let RDS_PORT;
+let RDS_USERNAME;
+let RDS_PASSWORD;
+let RDS_DB_NAME;
+let TYPEORM_SYNC;
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    TYPEORM_SYNC = false;
+  case 'development':
+    RDS_TYPE = 'postgres';
+    RDS_HOSTNAME = 'localhost';
+    RDS_PORT = 5432;
+    RDS_USERNAME = 'postgres';
+    RDS_PASSWORD = 'postgres';
+    RDS_DB_NAME = 'helpwanted';
+    TYPEORM_SYNC = true;
+  default:
+    RDS_TYPE = 'postgres';
+    RDS_HOSTNAME = 'localhost';
+    RDS_PORT = 5432;
+    RDS_USERNAME = 'postgres';
+    RDS_PASSWORD = 'postgres';
+    RDS_DB_NAME = 'helpwanted';
+    TYPEORM_SYNC = true;
+}
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type,
-  host: process.env.RDS_HOSTNAME || host,
-  port: process.env.RDS_PORT || port,
-  username: process.env.RDS_USERNAME || username,
-  password: process.env.RDS_PASSWORD || password,
-  database: process.env.RDS_DB_NAME || database,
+  type: RDS_TYPE,
+  host: process.env.RDS_HOSTNAME || RDS_HOSTNAME,
+  port: process.env.RDS_PORT || RDS_PORT,
+  username: process.env.RDS_USERNAME || RDS_USERNAME,
+  password: process.env.RDS_PASSWORD || RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME || RDS_DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.TYPEORM_SYNC || synchronize,
+  synchronize: process.env.TYPEORM_SYNC || TYPEORM_SYNC,
 };
