6df09dba795a7602d9ef99b225b5f2904b581b95
jchung201 <jchung201@gmail.com>
2020-04-02 16:51:57 -0400

    (App) Routes for nextjs created.

--- server/app.controller.ts
+++ server/app.controller.ts
@@ -11,16 +11,47 @@ export class AppController {
     @Req() req: IncomingMessage,
     @Res() res: ServerResponse,
   ) {
-    // this will render `pages/index.tsx`!
     await this.next.render('/index', req, res);
   }
 
+  @Get('categories')
+  public async getCategories(
+    @Req() req: IncomingMessage,
+    @Res() res: ServerResponse,
+  ) {
+    await this.next.render('/categories', req, res);
+  }
+
   @Get('posts')
-  public async showPostings(
+  public async showPosts(
     @Req() req: IncomingMessage,
     @Res() res: ServerResponse,
   ) {
     // this will render `pages/index.tsx`!
     await this.next.render('/posts', req, res);
+  }
+
+  @Get('posts/:id')
+  public async showPost(
+    @Req() req: IncomingMessage,
+    @Res() res: ServerResponse,
+  ) {
+    await this.next.render('/posts/:id', req, res);
+  }
+
+  @Get('create')
+  public async createPost(
+    @Req() req: IncomingMessage,
+    @Res() res: ServerResponse,
+  ) {
+    await this.next.render('/create', req, res);
+  }
+
+  @Get('edit/:id')
+  public async editPost(
+    @Req() req: IncomingMessage,
+    @Res() res: ServerResponse,
+  ) {
+    await this.next.render('/edit/:id', req, res);
   }
 }
