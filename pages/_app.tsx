5004eb36006970048c58c4fb18e99fc16b6f66e3
jchung201 <jchung201@gmail.com>
2020-04-04 02:26:43 -0400

    (color/_app) Fixed props for _app and added theme provider to components.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Head from 'next/head';
-export default ({ Component, pageProps }) => (
-  <React.Fragment>
-    <Head>
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <title>TownBB | Classifieds</title>
-    </Head>
-    <Component {...pageProps} />
-  </React.Fragment>
-);
+import App from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import theme from '../components/styled/theme';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <title>TownBB | Classifieds</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </React.Fragment>
+    );
+  }
+}
+export default MyApp;
