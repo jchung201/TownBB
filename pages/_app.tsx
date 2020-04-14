7e203c8fc99861eeb71ce77567536b9dbec2709c
jchung201 <jchung201@gmail.com>
2020-04-14 01:50:50 -0400

    (Assets) Changed to new design.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,7 @@ class MyApp extends App<ReduxWrapperAppProps<RootState>> {
     return (
       <React.Fragment>
         <Head>
-          <title>TownBB | Classifieds</title>
+          <title>TownBB | Local Jobs</title>
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
