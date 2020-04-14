f001d47e2b4d456daa12b92232062b6a91c42f6b
jchung201 <jchung201@gmail.com>
2020-04-14 02:59:43 -0400

    (List) Using links now and changed styling.

--- components/Home/List.tsx
+++ components/Home/List.tsx
@@ -1,7 +1,9 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import moment from 'moment';
+import Link from 'next/link';
 import { useSelector } from 'react-redux';
-import { useRouter } from 'next/router';
+import { makeStyles } from '@material-ui/core/styles';
+
 import {
   Card,
   CardContent,
@@ -12,21 +14,29 @@ import {
 import BusinessIcon from '@material-ui/icons/Business';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
+const useStyles = makeStyles(theme => ({
+  listImage: {
+    width: '12em',
+    margin: '1em',
+    paddingRight: '3em',
+    [theme.breakpoints.down('sm')]: {
+      width: '8em',
+    },
+  },
+}));
+
 const List = ({ width }) => {
   const posts = useSelector(state => state.home.posts);
   const type = useSelector(state => state.home.type);
-  const router = useRouter();
-  const [postId, setPost] = useState('');
-  if (postId) {
-    router.push(`/posts/${postId}`);
-  }
+  const classes = useStyles();
+
   let descriptionWidth = 35;
   if (isWidthUp('lg', width)) {
     descriptionWidth = 100;
   } else if (isWidthUp('md', width)) {
-    descriptionWidth = 65;
+    descriptionWidth = 50;
   } else if (isWidthUp('sm', width)) {
-    descriptionWidth = 55;
+    descriptionWidth = 40;
   }
   let finalPosts;
   if (type && type !== 'All') {
@@ -40,65 +50,59 @@ const List = ({ width }) => {
     <Fragment>
       {finalPosts.map(post => {
         return (
-          <Card
-            style={{ display: 'flex', marginTop: '2em', cursor: 'pointer' }}
-            key={post.id}
-            onClick={() => {
-              setPost(post.id);
-            }}
-          >
-            <CardMedia
-              style={{
-                minWidth: '8em',
-                width: '8em',
-                margin: '1em',
-                paddingRight: '3em',
-              }}
-              image={
-                post.images.length > 0
-                  ? post.images[0]
-                  : 'https://cms.prod.nypr.digital/images/297736/fill-661x496/'
-              }
-              title="Live from space album cover"
-            />
-            <CardContent style={{ flex: '1 0 auto' }}>
-              <Typography
-                component="h4"
-                variant="h4"
-                style={{ marginBottom: '0.2em' }}
-              >
-                {post.title}
-              </Typography>
-              {post.company && (
-                <Chip
-                  icon={<BusinessIcon />}
-                  size="small"
-                  label={post.company}
-                  style={{ marginRight: '1em' }}
-                />
-              )}
-              <span>
-                <Chip label={post.location} size="small" variant="outlined" />
-              </span>
-              <Typography
-                component="h6"
-                variant="h6"
-                style={{ marginTop: '0.2em' }}
-              >
-                {post.value}
-              </Typography>
-              <Typography variant="subtitle1">
-                {post.description.substring(0, descriptionWidth) + '...'}
-              </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                Last updated:{' '}
-                {moment
-                  .utc(post.updated)
-                  .local()
-                  .format('lll')}
-              </Typography>
-            </CardContent>
-          </Card>
+          <Link href={`/posts/${post.id}`} key={post.id}>
+            <Card
+              style={{ display: 'flex', marginTop: '2em', cursor: 'pointer' }}
+              component="a"
+            >
+              <CardMedia
+                className={classes.listImage}
+                image={
+                  post.images.length > 0
+                    ? post.images[0]
+                    : 'https://cms.prod.nypr.digital/images/297736/fill-661x496/'
+                }
+                title="Card Picture"
+              />
+              <CardContent style={{ flex: '1 0 auto' }}>
+                <Typography
+                  component="h4"
+                  variant="h4"
+                  style={{ marginBottom: '0.2em' }}
+                >
+                  {post.title}
+                </Typography>
+                {post.company && (
+                  <Chip
+                    icon={<BusinessIcon />}
+                    size="small"
+                    label={post.company}
+                    style={{ marginRight: '1em' }}
+                  />
+                )}
+                <span>
+                  <Chip label={post.location} size="small" variant="outlined" />
+                </span>
+                <Typography
+                  component="h6"
+                  variant="h6"
+                  style={{ marginTop: '0.2em' }}
+                >
+                  {post.value}
+                </Typography>
+                <Typography variant="subtitle1">
+                  {post.description.substring(0, descriptionWidth) + '...'}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                  Last updated:{' '}
+                  {moment
+                    .utc(post.updated)
+                    .local()
+                    .format('lll')}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
         );
       })}
       {posts.length <= 0 && (
