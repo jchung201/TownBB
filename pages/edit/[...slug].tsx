df8c540db271befa87b216ce5c0cb4152766ad6f
jchung201 <jchung201@gmail.com>
2020-04-14 02:15:08 -0400

    (Fix) Typo.

--- pages/edit/[...slug].tsx
+++ pages/edit/[...slug].tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Error from 'next/error';
 
 import CreateContainer from '../../components/Create/CreateContainer';
-import { API_URL } from '../../utilities/envUrl;
+import { API_URL } from '../../utilities/envUrl';
 
 const EditPost = ({ foundPost, err }) => {
   if (err) return <Error statusCode={err.statusCode} />;
@@ -13,14 +13,11 @@ const EditPost = ({ foundPost, err }) => {
 EditPost.getInitialProps = async ({ query: { slug } }) => {
   try {
     const hash = slug[1];
-    const foundPost = await axios.get(
-      `${API_URL}/rest/ads/${slug[0]}/check`,
-      {
-        params: {
-          hash,
-        },
+    const foundPost = await axios.get(`${API_URL}/rest/ads/${slug[0]}/check`, {
+      params: {
+        hash,
       },
-    );
+    });
     return {
       foundPost: foundPost.data,
     };
