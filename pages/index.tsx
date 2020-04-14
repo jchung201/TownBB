d2935aac856bf866a7813cded5cc4b6f33f081b0
jchung201 <jchung201@gmail.com>
2020-04-14 04:16:02 -0400

    (Filter) Category back fix.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomeContainer from '../components/Home/HomeContainer';
 import { getPosts, getCategories, getLocation } from '../store/home/home.GIP';
-import { setCategoryAction } from '../store/home/homeActions';
+import { setCategoryAction, setTypeAction } from '../store/home/homeActions';
 const Home = () => {
   return <HomeContainer />;
 };
@@ -11,6 +11,7 @@ Home.getInitialProps = async ({ store }) => {
   store.dispatch(await getLocation());
   store.dispatch(await getPosts());
   store.dispatch(setCategoryAction(null));
+  store.dispatch(setTypeAction('All'));
 };
 
 export default Home;
