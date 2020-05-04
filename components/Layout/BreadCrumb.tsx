824ea3fa6bf8059b5d572b4df1cce55e4d20cdc6
jchung201 <jchung201@gmail.com>
2020-05-03 23:00:43 -0400

    (Breadcrumb) Replaced with go back and section title.

--- components/Layout/BreadCrumb.tsx
+++ components/Layout/BreadCrumb.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { LinkB } from '../Common/Links';
-import { Breadcrumbs, Typography } from '@material-ui/core';
+import { Breadcrumbs, Typography, Fab } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const BreadCrumb = () => {
   const router = useRouter();
@@ -11,34 +12,51 @@ const BreadCrumb = () => {
   } = router;
 
   return (
-    <Breadcrumbs aria-label="breadcrumb">
-      <Link href="/">
-        <LinkB>
-          <Typography color="textPrimary">Home</Typography>
-        </LinkB>
-      </Link>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      {(router.pathname.startsWith('/create') ||
+        router.pathname.startsWith('/edit') ||
+        router.pathname.startsWith('/posts') ||
+        router.pathname.startsWith('/categories') ||
+        router.pathname.startsWith('/unsub')) && (
+        <Fab
+          variant="extended"
+          size="small"
+          color="primary"
+          style={{ marginRight: '1em' }}
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <ArrowBackIosIcon />
+          Back
+        </Fab>
+      )}
       {router.pathname.startsWith('/create') && (
-        <Typography color="textPrimary">Create Job Post</Typography>
+        <Typography variant="h6" color="textPrimary">
+          Create Job Post
+        </Typography>
       )}
       {router.pathname.startsWith('/edit') && (
-        <Typography color="textPrimary">
+        <Typography variant="h6" color="textPrimary">
           Edit Post: {String(slug[0])}
         </Typography>
       )}
       {router.pathname.startsWith('/posts') && (
-        <Typography color="textPrimary">{`Post: ${String(id)}`}</Typography>
+        <Typography variant="h6" color="textPrimary">{`Post: ${String(
+          id,
+        )}`}</Typography>
       )}
       {router.pathname.startsWith('/categories') && (
-        <Typography color="textPrimary">
-          {String(id)
-            .split('_')
-            .join(' ')}
+        <Typography variant="h6" color="textPrimary">
+          {String(id).split('_').join(' ')}
         </Typography>
       )}
       {router.pathname.startsWith('/unsub') && (
-        <Typography color="textPrimary">Unsubscribe</Typography>
+        <Typography variant="h6" color="textPrimary">
+          Unsubscribe
+        </Typography>
       )}
-    </Breadcrumbs>
+    </div>
   );
 };
 
